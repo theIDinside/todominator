2e8486d26c3945457aaa90222094a4aabb1821aa
Simon Farre <simon.farre.cx@gmail.com>
2022-01-03 10:57:01 +0100

    added simple test workspace

---
+++ testworkspace/src/main.rs
@@ -0,0 +1,11 @@
+pub fn foo() {
+    println!("foo!");
+}
+
+fn main() {
+    // todo(simon): implement a test for parsing
+    // fixme(simon): maybe clean this code up
+    // bug(simon): possible bug description
+    // feature_request(simon)!!!!: display the nota bene's in a nice list in vscode
+    println!("This should not trigger todo, fixme, bug or feature_request");
+}
